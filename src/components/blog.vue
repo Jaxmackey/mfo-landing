b57a73dd2823424578c8b1df0ebc3438c8452011
crystallinge <crystallinge@gmail.com>
2021-02-02 06:53:15 +0300

    add test router

---
+++ src/components/blog.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>123123</div>
+</template>
+
+<script>
+export default {}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueYandexMetrika from 'vue-yandex-metrika'
+import Blog from './components/blog'
 
+Vue.use(VueRouter)
 Vue.use(VueYandexMetrika, {
   id: 71367445,
   clickmap: true,
@@ -15,7 +18,17 @@ Vue.use(VueYandexMetrika, {
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '', component: App },
+  { path: '/blog', component: Blog }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
