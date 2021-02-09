4c591fdd01db0d027c6f391f26c6d77b4fe27929
Romanovski Yan <romanovski.dev@gmail.com>
2021-02-09 10:17:43 +0300

    fix route

--- src/main.js
+++ src/main.js
@@ -3,35 +3,36 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueYandexMetrika from 'vue-yandex-metrika'
-import index from './pages/index'
-import catalog from '@/pages/catalog'
-import blog from './pages/blog'
 
 Vue.use(VueRouter)
 Vue.use(VueYandexMetrika, {
   id: 71367445,
   scriptSrc: 'https://mc.yandex.ru/metrika/tag.js',
-  // env: 'production',
   options: {
     clickmap: true,
     trackLinks: true,
     accurateTrackBounce: true,
-    webvisor: true
+    webvisor: true,
+    env: 'dev'
   }
 })
 
 Vue.config.productionTip = false
 
-const router = new VueRouter({
-  routes: [
-    { path: '/', component: index },
-    { path: '/catalog', component: catalog },
-    { path: '/blog', component: blog }
-  ]
-})
+export function createRouter () {
+  return new VueRouter({
+    mode: 'history',
+    routes: [
+      { path: '/', component: () => import('./pages/index.vue') },
+      { path: '/catalog', component: () => import('./pages/catalog.vue') },
+      { path: '/blog', component: () => import('./pages/blog.vue') }
+    ]
+  })
+}
+const router = createRouter()
 
 new Vue({
-  router,
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
