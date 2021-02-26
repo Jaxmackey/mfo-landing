55dbc5b6197e8bcbd6d0c374c23f227a36963ed3
Jax Mackey <gimaglobin@gmail.com>
2021-02-26 11:28:31 +0300

    fix route

--- src/components/blog-single.vue
+++ src/components/blog-single.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   props: {
     itemInfo: null
@@ -14,6 +15,21 @@ export default {
   methods: {
     getImgUrl (pic) {
       return require(`@/assets/images/article/${pic}`)
+    },
+    getInfo () {
+      axios
+        .get(`https://ez-cash.ru/fullPostRoutController.php?name=${this.$route.params.name}`)
+        .then(({ data }) => {
+          this.itemInfo = data[0]
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  mounted () {
+    if (this.itemInfo === null) {
+      this.getInfo()
     }
   }
 }
--- src/pages/blogSingle.vue
+++ src/pages/blogSingle.vue
@@ -1,33 +1,36 @@
 <template>
-  <BlogSingle :itemInfo="itemInfo" />
+  <div v-if="itemInfo">
+    <div>{{ itemInfo.Title }}</div>
+    <div v-if="itemInfo.Img"><img :src="getImgUrl(itemInfo.Img)" :alt="itemInfo.Title"></div>
+    <div v-if="itemInfo.Html" v-html="itemInfo.Html" />
+  </div>
 </template>
 
 <script>
-import BlogSingle from '@/components/blog-single'
 import axios from 'axios'
 export default {
-  components: {
-    BlogSingle
-  },
   data: () => {
     return {
       itemInfo: null
     }
   },
   mounted () {
-    console.log(this.$route)
+    console.log(this.itemInfo)
     this.getInfo()
   },
   methods: {
     getInfo () {
-      axios
-        .get(`https://ez-cash.ru/fullpostController.php?article=${this.$route.params.id}`)
+      axios.get(`https://ez-cash.ru/fullPostRoutController.php?name=${this.$route.params.name}`)
         .then(({ data }) => {
+          console.log(data)
           this.itemInfo = data[0]
         })
         .catch(error => {
           console.log(error)
         })
+    },
+    getImgUrl (pic) {
+      return require(`@/assets/images/article/${pic}`)
     }
   }
 }
