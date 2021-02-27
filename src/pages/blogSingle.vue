a8659b7d05689fbb549babb227c63e9079496d11
Romanovski Yan <romanovski.dev@gmail.com>
2021-02-27 12:47:53 +0300

    fix css to duble single blog page

--- src/pages/blogSingle.vue
+++ src/pages/blogSingle.vue
@@ -1,8 +1,11 @@
 <template>
-  <div v-if="itemInfo">
-    <div>{{ itemInfo.Title }}</div>
-    <div v-if="itemInfo.Img"><img :src="getImgUrl(itemInfo.Img)" :alt="itemInfo.Title"></div>
-    <div v-if="itemInfo.Html" v-html="itemInfo.Html" />
+  <div class="container" v-if="itemInfo">
+    <Breadcrumbs />
+    <div v-if="itemInfo.Img" class="blog-img"><img :src="getImgUrl(itemInfo.Img)" :alt="itemInfo.Title"></div>
+    <div class="blog-content">
+      <h1 class="h1-black">{{ itemInfo.Title }}</h1>
+      <div v-if="itemInfo.Html" v-html="itemInfo.Html" class="blog-text"></div>
+    </div>
   </div>
 </template>
 
@@ -35,3 +38,34 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.blog-img {
+  border-radius: 50px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    max-width: 100%;
+  }
+}
+
+.blog-content {
+  max-width: 850px;
+  margin: 0 auto 50px;
+}
+
+.blog-text {
+  font-size: 22px;
+  line-height: 30px;
+  display: flex;
+  flex-direction: column;
+
+  @media(max-width: 767px) {
+    font-size: 16px;
+    line-height: 22px;
+  }
+}
+</style>
